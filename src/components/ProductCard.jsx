eb098f47399018acbb3bb2bc68cb7f16ac4fdea5
gidzkidz5 <124338423+gidzkidz5@users.noreply.github.com>
2023-06-14 11:20:03 +1000

    final

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,12 @@
+// import appDesign from "../assets/app-design";
+// import graphicDesign from "../assets/graphic-design"
+// import webDesign from "../assets/web-design"
+
 export default function ProductCard({ title, description, category }) {
 
     return (
         <div className="flex product-card">
-            <img src={"../src/assets/" + category +"-design/desktop/image-" + title.split(' ').join('').replace('!','') + ".jpg"}/>
+            <img src={"../public/" + category +"-design/desktop/image-" + title.split(' ').join('').replace('!','') + ".jpg"}/>
             <div className="ff-sanserif product-card-text-container flex">
                 <h1 className="uppercase fs-S">{title}</h1>
                 <h2 className="fs-body font-black">{description}</h2>
